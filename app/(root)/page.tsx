d7f6ee0d09da4d22b2beda2e04338f2c9f5939a5
Emir Risyad <emirrsyd@gmail.com>
2023-09-29 22:26:37 +0200

    clerk setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <p>Hello admin dashboard</p>;
-}
+import { UserButton } from '@clerk/nextjs';
+
+const SetupPage = () => {
+  return (
+    <div className='p-4'>
+      <UserButton afterSignOutUrl='/' />
+    </div>
+  );
+};
+
+export default SetupPage;
